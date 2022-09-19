4e193cd67a974f6a9230ba147a41c6a26d302e41
xmh0511 <970252187@qq.com>
2022-09-19 21:08:24 +0800

    update

--- src/http_parser/websocket.rs
+++ src/http_parser/websocket.rs
@@ -439,7 +439,7 @@ fn switch_to_websocket(
                         }
                         let first_byte = buff[0];
                         let fin = (first_byte >> 7) & 1; // 1是最后一个包或完整的包, 0是分包
-                        println!("first_entry:{first_entry}, first_byte:{:b}",first_byte);
+                        println!("first_entry:{first_entry}, first_byte:{:b}, second_byte:{:b}",first_byte,buff[1]);
                         if first_entry {
                             opcode = first_byte & 0b00001111u8; //如果是分片传输，只记录首次的frame中的opcode
                             first_entry = false;
